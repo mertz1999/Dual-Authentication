0763a2abce8b1c8ce262f2b53b51ba4ab02b8218
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-05 18:33:30 +0430

    (FEAT) Implement enhancement method

---
+++ iris_src/enhancement.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 12:35:42 2018
+@author: user
+"""
+
+import numpy as np
+import cv2
+
+def enhancement(row):
+    img = row['Image']
+    dim = img.shape
+    
+    stride = 16  
+    initialize_img = np.zeros((int(dim[0]/stride), int(dim[1]/stride)))
+    
+    for i in range(0,dim[0]-15,stride):
+        for j in range(0,dim[1]-15,stride):
+            block = img[i:i+stride, j:j+stride]
+            m = np.mean(block,dtype=np.float32)
+            initialize_img[i//16, j//16] = m
+            
+    image_set = cv2.resize(initialize_img, (dim[1],dim[0]), interpolation=cv2.INTER_CUBIC)
+
+    enhance = img - image_set
+    enhance = enhance - np.amin(enhance.ravel())
+    img = enhance.astype(np.uint8)      
+         
+    img2 = np.zeros(dim)
+    for i in range(0,img.shape[0],stride*2):
+        for j in range(0,img.shape[1],stride*2):
+            img2[i:i+stride*2, j:j+stride*2] = cv2.equalizeHist(img[i:i+stride*2, j:j+stride*2].astype(np.uint8))
+    cv2.imwrite('enhancement_result.png', img2)
+
+    return img2
