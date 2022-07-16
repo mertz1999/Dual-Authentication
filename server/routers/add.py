054f4e4830a1556ca173b9c9af673bfe5875e08d
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-16 20:48:11 +0430

    (FEAT) Use database in add method

--- server/routers/add.py
+++ server/routers/add.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, UploadFile, File
 from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
 import numpy as np
 import random
 import time
@@ -9,6 +10,11 @@ import os
 from core.face import FaceRecognition
 from core.iris import IrisRecognition
 from core.setting import *
+from server import database, models
+
+from pydantic import BaseModel
+from typing import Union
+
 
 # An instance of face-recognition
 instace_face = FaceRecognition(MODELS, METRICS)
@@ -23,13 +29,17 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def add(
-    face_file   : UploadFile = File(description="Upload your Face image"),
-    iris_file_1 : UploadFile = File(description="Upload your Iris image (1)"),
-    iris_file_2 : UploadFile = File(description="Upload your Iris image (2)")
+    name_user   : str, 
+    face_file   : UploadFile = File(description="Upload Face image"),
+    iris_file_1 : UploadFile = File(description="Upload Iris 1 image"),
+    iris_file_2 : UploadFile = File(description="Upload Iris 2 image"),
+    db: Session = Depends(database.get_db)
     ):
 
+
+
+    # Read image contents
     try:
-        # Read image contents
         face_contents   = await face_file.read()
         iris_1_contents = await iris_file_1.read()
         iris_2_contents = await iris_file_2.read()
@@ -48,29 +58,34 @@ async def add(
         nparr      = np.fromstring(iris_2_contents, np.uint8)
         iris_2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         cv2.imwrite(name+'_iris_2.png', iris_2_img)
+    except:
+        return {"detail" : "Error in reading data"}
 
-        # Embedding all images
+    # Embedding all images
+    try:
         face_embed   = instace_face.embedding(name+'_face.png')
         iris_1_embed = instace_iris.embedding(name+'_iris_1.png')
         iris_2_embed = instace_iris.embedding(name+'_iris_2.png')
-
-        print(iris_2_embed)
 
         # Remove files
         os.remove(name+'_face.png')
         os.remove(name+'_iris_1.png')
         os.remove(name+'_iris_2.png')
-
-        return "done!"
-        # return {
-        #     'face': face_embed,
-        #     'iris_1' : iris_1_embed,
-        #     'iris_2' : iris_2_embed
-        # }
-
-
     except:
-        return {"message": "There was an error uploading the file"}
+        return {"detail": "Error in embedding"}
+
+    # Add new User 
+    try:
+        new_user = models.User(name=name_user, face=str(face_embed['Facenet']), iris_1= str(iris_1_embed), iris_2= str(iris_2_embed))
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return {"detail" : f"Information accepted and pushed to databse with name ({name_user})"}
+    except:
+        return {"detail" : "Problem in adding user to database"}
+    
+
 
 
 # @router.get('/')
