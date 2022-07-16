7f5530274a07d2d5cd41e8896039528a0139c96f
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-16 20:47:15 +0430

    (FEAT) Using database and initialize it

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from fastapi import Depends, FastAPI, status, HTTPException
 from pydantic import BaseModel
 
 from server.routers import add
+from server import models, database
+
+import os 
+import pathlib
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=database.engine)
+
 app.include_router(add.router)
 
 # embed_1 = face.embedding('./images/face/1.png')
